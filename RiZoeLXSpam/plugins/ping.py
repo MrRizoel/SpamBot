b53b5b16a768a7df8b2c23859cf896afff640808
The RiZoeL <85222112+MrRizoel@users.noreply.github.com>
2021-11-28 15:29:17 +0530

    Update ping.py

--- RiZoeLXSpam/plugins/ping.py
+++ RiZoeLXSpam/plugins/ping.py
@@ -50,4 +50,9 @@ async def ping(e):
         event = await e.reply(text, parse_mode=None, link_preview=None )
         end = datetime.now()
         ms = (end-start).microseconds / 1000
-        await event.edit(f"█▀█ █▀█ █▄░█ █▀▀\n█▀▀ █▄█ █░▀█ █▄█\n\nϟ ʀɪᴢᴏᴇʟ X sᴘᴀᴍ ϟ︎ `{ms}` ᴍs")                       
+        await event.edit(f"▒█▀▀█ ▒█▀▀▀█ ▀▀█▀▀\n▒█▀▀▄ ▒█░░▒█ ░▒█░░\n▒█▄▄█ ▒█▄▄▄█ ░▒█░░\n\nϟ ʀɪᴢᴏᴇʟ X sᴘᴀᴍ ϟ︎ `{ms}` ᴍs")                       
+
+
+# ▒█▀▀█ ▒█▀▀▀█ ▀▀█▀▀
+# ▒█▀▀▄ ▒█░░▒█ ░▒█░░
+# ▒█▄▄█ ▒█▄▄▄█ ░▒█░░
