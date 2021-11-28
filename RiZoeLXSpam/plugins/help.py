a698151c664e2711aa3eb0512e4449de0330eeed
The RiZoeL <85222112+MrRizoel@users.noreply.github.com>
2021-11-28 16:41:39 +0530

    UPDATE [Help.py]

--- RiZoeLXSpam/plugins/help.py
+++ RiZoeLXSpam/plugins/help.py
@@ -34,7 +34,9 @@ async def help(event):
                                   caption=Riz_Help,
                                   buttons=[
         [
-        Button.url("ᴀʟʟ ᴄᴍᴅs", "https://telegra.ph/%F0%9D%97%A5%F0%9D%97%9C%F0%9D%97%AD%F0%9D%97%A2%F0%9D%97%98%F0%9D%97%9F-%F0%9D%97%AB-%F0%9D%97%A6%F0%9D%97%A3%F0%9D%97%94%F0%9D%97%A0-11-28-2"),
+        Button.url("ᴀʟʟ ᴄᴍᴅs", "https://telegra.ph/%F0%9D%97%A5%F0%9D%97%9C%F0%9D%97%AD%F0%9D%97%A2%F0%9D%97%98%F0%9D%97%9F-%F0%9D%97%AB-%F0%9D%97%A6%F0%9D%97%A3%F0%9D%97%94%F0%9D%97%A0-11-28-2")
+        ],
+        [
         Button.url("ᴄʜᴀɴɴᴇʟ", "https://t.me/RiZoeLX")
         ] 
         ]
