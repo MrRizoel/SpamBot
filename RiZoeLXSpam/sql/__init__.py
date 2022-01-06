c323ef938695fec6be877e2c7435e28aab7181e7
The RiZoeL <85222112+MrRizoel@users.noreply.github.com>
2022-01-07 00:36:45 +0530

    ADD FILES

---
+++ RiZoeLXSpam/sql/__init__.py
@@ -0,0 +1,26 @@
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+# the secret configuration specific things
+from RiZoeLXSpam import DB_URI
+
+
+def start() -> scoped_session:
+    engine = create_engine(DB_URI)
+    BASE.metadata.bind = engine
+    BASE.metadata.create_all(engine)
+    return scoped_session(sessionmaker(bind=engine, autoflush=False))
+
+
+try:
+    BASE = declarative_base()
+    SESSION = start()
+except AttributeError as e:
+    # this is a dirty way for the work-around required for #23
+    print(
+        "DB_URI is not configured. Features depending on the database might have issues."
+    )
+    print(str(e))
